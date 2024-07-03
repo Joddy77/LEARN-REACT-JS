78a12e204a6cfd0e71d2c08d2276f41cc5c0a598
Joddy77 <joddys77@gmail.com>
2024-07-03 19:21:08 +0700

    [BELAJAR HTTPS Get]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import SignUpPage from "./Pages/SignUpPage";
+import WishlistPage from "./Pages/WishlistPage";
 
 function App() {
-  return <SignUpPage />
+   return <SignUpPage />
+  // return <WishlistPage />
 }
 
 export default App;
--- src/Pages/WishlistPage.jsx
+++ src/Pages/WishlistPage.jsx
@@ -1,14 +1,25 @@
 import { Button, Input } from "@nextui-org/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../lib/axios";
 
 const WishlistPage = () => {
   const [wishlistInput, setWishlistInput] = useState("");
   const [wishlistItems, setWIshlistItems] = useState([]);
 
+  const fetchWishlistItems = async () => {
+    const respons = await axiosInstance.get("/wishilist-items")
+
+    setWIshlistItems(respons.data);
+  }
+
   const addWishlist = () => {
     setWIshlistItems([...wishlistItems, wishlistInput]);
     setWishlistInput("");
   };
+
+  useEffect(() => {
+    fetchWishlistItems();
+  }, [])
 
   return (
     <>
@@ -30,7 +41,7 @@ const WishlistPage = () => {
 
       <ul className="list-decimal list-inside text-center">
         {wishlistItems.map((item) => {
-          return <li>{item}</li>;
+          return <li>{item.name}</li>;
         })}
       </ul>
     </>
